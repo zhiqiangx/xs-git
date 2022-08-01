2bf39edc9c75270c97a5314c524be3cadc13001a
zhiqiangx <13127955995@163.com>
2022-08-01 23:30:09 +0800

    feat: test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const git = require("git-promise");
 
 const main = async (args) => {
   if (Array.isArray(args)) {
-    const addReq = await git('add .');
-    console.log('addreq', addReq);
+    const pullMsg = await git('log');
+    console.log('addreq', pullMsg);
   }
 }
 
