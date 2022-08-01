dafa7780cd3189a66f63eb76073d56b5bb1ea473
zhiqiangx <13127955995@163.com>
2022-08-01 23:34:13 +0800

    feat: get commit message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const git = require("git-promise");
 
 const main = async (args) => {
   if (Array.isArray(args)) {
-    const pullMsg = await git('log');
+    const pullMsg = await git('log --pretty=format:--split--"%s"');
+    console.log('arr', pullMsg.split('--split--'));
     console.log('addreq', pullMsg);
   }
 }
